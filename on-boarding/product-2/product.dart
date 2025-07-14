3b9b6910163316b078531494d9eaa852e0350d94
mahlet <mahlet2ambaye@gmail.com>
2025-07-14 14:53:00 +0300

    fix : add comment and use getter and setter

--- on-boarding/product-2/product.dart
+++ on-boarding/product-2/product.dart
@@ -3,10 +3,19 @@ class Product {
   String Description;
   double price;
 
-  Product(
-    {
-    required this.Name, 
-    required this.Description,
-     required this.price
-     });
+  Product({required this.Name, required this.Description, required this.price});
+
+  //setter
+  set setName(String name) => this.Name = name;
+
+  set setDescription(String description) => this.Description = description;
+
+  set setPrice(double price) => this.price = price;
+
+  //getter
+  String get getName => this.Name;
+
+  String get getDescription => this.Description;
+
+  double get getPrice => this.price;
 }
--- on-boarding/product-2/product_manager.dart
+++ on-boarding/product-2/product_manager.dart
@@ -3,55 +3,64 @@ import 'product.dart';
 class ProductManager {
   List<Product> products = [];
 
+
+// add a product to the list
   void addProduct(Product product) {
     products.add(product);
+    print('Product "${product.getName}" added.');
   }
 
+  // inorder to display a products name,discription and price
   void display(Product product) {
-    
-    print('Name: ${product.Name}');
-    print('Description: ${product.Description}');
-    print('price: ${product.price}');
+    print('Name: ${product.getName}');
+    print('Description: ${product.getDescription}');
+    print('price: ${product.getPrice}');
   }
 
+ // to display all products
   void viewallProducts() {
     if (products.isEmpty) {
       print('No products available');
-      
-    }else{
-    for (var product in products) {
-      display(product);
-    }
+    } else {
+      for (var product in products) {
+        display(product);
+      }
     }
   }
 
+// inorder to view a single product 
   void viewsingleProduct(String name) {
     try {
-      var product = products.firstWhere((Product) => Product.Name == name);
+      var product = products.firstWhere((Product) => Product.getName == name);
       display(product);
     } catch (e) {
       print('this product not found');
     }
   }
 
-  void editproduct(String name, String newdescription, double newprice , String newname) {
+// inorder to edit a product name,discription and price
+  void editproduct(
+    String name,
+    String newdescription,
+    double newprice,
+    String newname,
+  ) {
     try {
-      var product = products.firstWhere((Product) => Product.Name == name);
-      product.Description = newdescription;
-      product.price = newprice;
-      product.Name = newname;
+      var product = products.firstWhere((Product) => Product.getName == name);
+      product.setDescription = newdescription;
+      product.setPrice = newprice;
+      product.setName = newname;
       display(product);
     } catch (e) {
       print('this product not found');
     }
-
-
   }
 
+// inorder to remove a product from the list    
   void deleteProduct(String name) {
     try {
       var product = products.firstWhere((Product) {
-        return Product.Name == name;
+        return Product.getName == name;
       });
       products.remove(product);
       display(product);
@@ -59,5 +68,4 @@ class ProductManager {
       print('product not found');
     }
   }
-
 }
